4f4d627e17daeb5448661cf5914207462a3560fc
Arisean <arisean.miller@hotmail.com>
2017-06-08 13:38:21 -0500

    Refactor: Updated Navbar

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $(".logo__image").click(function() {
     $(".logo__arrow").toggleClass("logo__arrow-open");
     $(".nav__item").toggleClass("nav__item-open");
+    $(".navigation").toggleClass("navigation-open");
   });
 
   //Submenu active link
