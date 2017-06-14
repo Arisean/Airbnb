835288a6a79b86631dad49e4d6ecc4395d70062d
Arisean <arisean.miller@hotmail.com>
2017-06-13 20:46:23 -0500

    Refactor: Seperated nav into 2 menus

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,22 @@
 // Navgiation Drop Down Menu
 $(document).ready(function() {
-  $(".logo__image").click(function() {
-    $(".logo__arrow").toggleClass("logo__arrow-open");
-    $(".nav__item").toggleClass("nav__item-open");
-    $(".navigation").toggleClass("navigation-open");
+
+  var logo = $('.logo__image');
+
+
+  // Fly in menu
+  logo.click(function() {
+
+    var arrow = $('.logo__arrow');
+
+    arrow.toggleClass("logo__arrow-open");
+    ghostLinks.toggle(display);
+    ghostHelp.toggle(display);
+
+
   });
 
+  // Hide full-width menu links
+
   //Submenu active link
-    $(".submenu__link").click(function() {
-      $(this).addClass('active');
-    });
 });
